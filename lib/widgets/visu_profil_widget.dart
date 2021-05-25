858ddb3c609e9bfa1cf23c20b4b5c9e0945417b6
Samuel Bagattin <samuel.bagattin@gmail.com>
2021-05-25 13:40:58 +0200

    Fix analysis and tests

--- lib/widgets/visu_profil_widget.dart
+++ lib/widgets/visu_profil_widget.dart
@@ -5,7 +5,6 @@ import '../backend/backend.dart';
 // import '../../flutter_flow/flutter_flow_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class VisuProfilWidget extends StatefulWidget {
   VisuProfilWidget({Key key}) : super(key: key);
